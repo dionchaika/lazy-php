3b36f9d3f0121122a69c845bc248518f97b32bfc
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-05 17:39:04 +0300

    Add functions

---
+++ src/Http/src/functions.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Lazy\Http;
+
+use Psr\Http\Message\MessageInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+if (! function_exists('to_string')) {
+    /**
+     * Get the string
+     * representation of the HTTP message.
+     *
+     * @param  Psr\Http\Message\MessageInterface  $message
+     *
+     * @return string
+     */
+    function to_string(MessageInterface $message): string
+    {
+        if ($message instanceof RequestInterface) {
+            $res = "{$message->getMethod()} {$message->getRequestTarget()} HTTP/{$message->getProtocolVersion()}\r\n";
+            foreach (array_keys($message->getHeaders()) as $header) {
+                if (0 === strcasecmp($header, 'cookie')) {
+                    $cookie = implode('; ', $message->getHeader('Cookie'));
+                    $res .= "{$header}: {$cookie}\r\n";
+                } else {
+                    $res .= "{$header}: {$message->getHeaderLine($header)}\r\n";
+                }
+            }
+        } else if ($message instanceof ResponseInterface) {
+            $res = "HTTP/{$message->getProtocolVersion()} {$message->getStatusCode()} {$message->getReasonPhrase()}\r\n";
+            foreach (array_keys($message->getHeaders()) as $header) {
+                if (0 === strcasecmp($header, 'set-cookie')) {
+                    foreach ($message->getHeader('Set-Cookie') as $setCookie) {
+                        $res .= "{$header}: {$setCookie}\r\n";
+                    }
+                } else {
+                    $res .= "{$header}: {$message->getHeaderLine($header)}\r\n";
+                }
+            }
+        }
+
+        return $res;
+    }
+}
