28af6dbb251870d41a56cb2258d6353faccc4bc5
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-05 16:17:03 +0300

    Add helper classes

---
+++ src/Http/src/Method.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Lazy\Http;
+
+/**
+ * The HTTP request method helper class.
+ *
+ * @see https://tools.ietf.org/html/rfc7230
+ */
+abstract class Method
+{
+    const GET     = 'GET';
+    const HEAD    = 'HEAD';
+    const POST    = 'POST';
+    const PUT     = 'PUT';
+    const PATCH   = 'PATCH';
+    const DELETE  = 'DELETE';
+    const OPTIONS = 'OPTIONS';
+    const TRACE   = 'TRACE';
+    const CONNECT = 'CONNECT';
+}
