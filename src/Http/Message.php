aca7802d243bef45d84acc10b0b975cff29252a3
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-10 15:12:58 +0300

    Update Message.php

--- src/Http/Message.php
+++ src/Http/Message.php
@@ -236,8 +236,10 @@ abstract class Message implements MessageInterface
             ];
         }
 
-        $this->headers[$normalizedName]['values']
-            = array_merge($this->headers[$normalizedName]['values'], $this->filterHeaderValue($value));
+        $this->headers[$normalizedName]['values'] = array_merge(
+            $this->headers[$normalizedName]['values'],
+            $this->filterHeaderValue($value)
+        );
     }
 
     /**
