01f5ade62381de33ea6eb50d61c708c866a8f149
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-11 15:59:24 +0300

    Update ClientTrait.php

--- src/Client/ClientTrait.php
+++ src/Client/ClientTrait.php
@@ -25,6 +25,8 @@ trait ClientTrait
     public function sendRequest(RequestInterface $request): ResponseInterface
     {
         $request = $this->prepareRequest($request);
+
+        $this->requestOrigin = $request->getUri()->getAuthority();
     }
 
     /**
@@ -35,6 +37,55 @@ trait ClientTrait
      */
     protected function prepareRequest(RequestInterface $request): RequestInterface
     {
+        foreach ($this->config['headers'] as $name => $value) {
+            if (! $request->hasHeader($name)) {
+                $request = $request->withHeader($name, $value);
+            }
+        }
+
+        if ($this->config['origin_header'] && ! $request->hasHeader('Origin')) {
+            $request = $request->withHeader('Origin', $this->requestOrigin);
+        }
+
+        if ('' === $request->getMethod()) {
+            $request = $request->withMethod('GET');
+        }
+
+        if ('' === $request->getProtocolVersion()) {
+            $request = $request->withProtocolVersion('1.1');
+        }
+
+        if ('1.1' === $request->getProtocolVersion()) {
+            $request = $request->withHeader('Connection', 'close');
+        }
+
+        if ('' === $request->getUri()->getScheme()) {
+            $request = $request->withUri($request->getUri()->withScheme('http'));
+        }
+
+        if ('' === $request->getUri()->getHost()) {
+            throw new RequestException($request, 'Invalid request URI! Host is not defined.');
+        }
+
+        if ($this->config['cookies']) {
+            $this->cookieStorage->clearExpiredCookies();
+            $request = $this->cookieStorage->includeToRequest($request);
+        }
+
+        if ($request->getBody()->getSize()) {
+            if ('GET' === $request->getMethod() || 'HEAD' === $request->getMethod()) {
+                throw new RequestException($request, 'Invalid request! Request with a "GET" or a "HEAD" method cannot contain a body.');
+            }
+
+            if (! $request->getBody()->isReadable()) {
+                throw new RequestException($request, 'Invalid request! Body is not readable.');
+            }
+
+            if (! $request->hasHeader('Content-Length')) {
+                $request = $request->withHeader('Content-Length', (string)$request->getBody()->getSize());
+            }
+        }
+
         return $request;
     }
 }
