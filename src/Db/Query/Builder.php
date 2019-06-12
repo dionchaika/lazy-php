006cd0b4aedad06316702d3359d5bbbe29fce8c2
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-12 12:33:12 +0300

    Update Builder.php

--- src/Db/Query/Builder.php
+++ src/Db/Query/Builder.php
@@ -4,5 +4,30 @@ namespace Lazy\Db\Query;
 
 class Builder
 {
-    
+    /**
+     * The builder table.
+     *
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * The builder compiler.
+     *
+     * @var Lazy\Db\Query\CompilerInterface
+     */
+    protected $compiler;
+
+    /**
+     * The query parts.
+     *
+     * @var mixed[]
+     */
+    protected $parts = [
+
+        'select'   => [],
+        'distinct' => false,
+        'from'     => null
+
+    ];
 }
