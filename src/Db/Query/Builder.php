8bb9693e3cabb78b26d9cb9c46cfa26891f56e0b
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-13 15:23:14 +0300

    Update Builder.php

--- src/Db/Query/Builder.php
+++ src/Db/Query/Builder.php
@@ -85,7 +85,11 @@ class Builder
     public function __construct(?string $db = null, ?string $table = null, ?CompilerInterface $compiler = null)
     {
         $this->db = $db;
-        $this->setTable($table);
+
+        if (null !== $table) {
+            $this->setTable($table);
+        }
+
         $this->compiler = $compiler ?? new BaseCompiler;
     }
 
