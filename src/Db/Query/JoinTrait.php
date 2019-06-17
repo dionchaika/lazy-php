71fc5621374026246b279b1ec9b3b0fdf3acb574
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-17 15:33:24 +0300

    Update JoinTrait.php

--- src/Db/Query/JoinTrait.php
+++ src/Db/Query/JoinTrait.php
@@ -10,4 +10,102 @@ trait JoinTrait
      * @var mixed[]
      */
     public $joins = [];
+
+    /**
+     * join...
+     *
+     * @param  string  $joinedTable
+     * @param  string  $firstCol
+     * @param  mixed  $op
+     * @param  mixed|null  $secondCol
+     * @param  string  $type
+     * @return \Lazy\Db\Query\Builder
+     */
+    public function join(string $joinedTable, string $firstCol, $op, $secondCol = null, string $type = 'Inner'): Builder
+    {
+        return $this->addJoin($joinedTable, $firstCol, $op, $secondCol, $type);
+    }
+
+    /**
+     * inner join...
+     *
+     * @param  string  $joinedTable
+     * @param  string  $firstCol
+     * @param  mixed  $op
+     * @param  mixed|null  $secondCol
+     * @return \Lazy\Db\Query\Builder
+     */
+    public function innerJoin(string $joinedTable, string $firstCol, $op, $secondCol = null): Builder
+    {
+        return $this->addJoin($joinedTable, $firstCol, $op, $secondCol, 'Inner');
+    }
+
+    /**
+     * left join...
+     *
+     * @param  string  $joinedTable
+     * @param  string  $firstCol
+     * @param  mixed  $op
+     * @param  mixed|null  $secondCol
+     * @return \Lazy\Db\Query\Builder
+     */
+    public function leftJoin(string $joinedTable, string $firstCol, $op, $secondCol = null): Builder
+    {
+        return $this->addJoin($joinedTable, $firstCol, $op, $secondCol, 'Left');
+    }
+
+    /**
+     * right join...
+     *
+     * @param  string  $joinedTable
+     * @param  string  $firstCol
+     * @param  mixed  $op
+     * @param  mixed|null  $secondCol
+     * @return \Lazy\Db\Query\Builder
+     */
+    public function rightJoin(string $joinedTable, string $firstCol, $op, $secondCol = null): Builder
+    {
+        return $this->addJoin($joinedTable, $firstCol, $op, $secondCol, 'Right');
+    }
+
+    /**
+     * full join...
+     *
+     * @param  string  $joinedTable
+     * @param  string  $firstCol
+     * @param  mixed  $op
+     * @param  mixed|null  $secondCol
+     * @return \Lazy\Db\Query\Builder
+     */
+    public function fullJoin(string $joinedTable, string $firstCol, $op, $secondCol = null): Builder
+    {
+        return $this->addJoin($joinedTable, $firstCol, $op, $secondCol, 'Full');
+    }
+
+    /**
+     * Add a query join clause.
+     *
+     * @param  string  $joinedTable
+     * @param  string  $firstCol
+     * @param  mixed  $op
+     * @param  mixed|null  $secondCol
+     * @param  string  $type
+     * @return \Lazy\Db\Query\Builder
+     */
+    protected function addJoin(string $joinedTable, string $firstCol, $op, $secondCol = null, string $type): Builder
+    {
+        [$op, $secondCol] = $this->prepareOpAndSecondCol($op, $secondCol);
+    }
+
+    /**
+     * Prepare an operation and a second column.
+     *
+     * @param  mixed  $op
+     * @param  mixed  $secondCol
+     * @return mixed[]
+     */
+    protected function prepareOpAndSecondCol($op, $secondCol): array
+    {
+        return (null === $secondCol) ? ['=', $op] : [$op, $secondCol];
+    }
 }
