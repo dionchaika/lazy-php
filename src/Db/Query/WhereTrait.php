2b9aa2302e552f203becb4d955e26f46ca35bc52
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-17 12:32:00 +0300

    Update WhereTrait.php

--- src/Db/Query/WhereTrait.php
+++ src/Db/Query/WhereTrait.php
@@ -10,4 +10,19 @@ trait WhereTrait
      * @var mixed[]
      */
     protected $wheres = [];
+
+    /**
+     * where...
+     *
+     * @param  mixed  $col
+     * @param  mixed|null  $op
+     * @param  mixed|null  $val
+     * @param  string  $delim
+     * @param  bool  $not
+     * @return \Lazy\Db\Query\Builder
+     */
+    public function where($col, $op = null, $val = null, string $delim = 'and', bool $not = false): Builder
+    {
+        return $this;
+    }
 }
