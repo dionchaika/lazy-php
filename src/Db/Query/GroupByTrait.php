36ab109fe8adfa763a899fc194a7ae263b448791
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-17 16:39:02 +0300

    Some fixes

--- src/Db/Query/GroupByTrait.php
+++ src/Db/Query/GroupByTrait.php
@@ -5,9 +5,25 @@ namespace Lazy\Db\Query;
 trait GroupByTrait
 {
     /**
-     * The array of query group by clauses.
+     * The array
+     * of query group by clauses.
      *
      * @var mixed[]
      */
     public $groupsBy = [];
+
+    /**
+     * group by...
+     *
+     * @param  mixed  $cols
+     * @return self
+     */
+    public function groupBy($cols): self
+    {
+        $this->groupsBy = is_array($cols)
+            ? $cols
+            : func_get_args();
+
+        return $this;
+    }
 }
--- src/Db/Query/HavingTrait.php
+++ src/Db/Query/HavingTrait.php
@@ -2,12 +2,48 @@
 
 namespace Lazy\Db\Query;
 
+/**
+ * @method mixed[] prepareOpAndVal($op, $val)
+ */
 trait HavingTrait
 {
     /**
-     * The array of query having clauses.
+     * The array
+     * of query having clauses.
      *
      * @var mixed[]
      */
     public $havings = [];
+
+    /**
+     * having...
+     *
+     * @param  string  $col
+     * @param  mixed  $op
+     * @param  mixed|null  $val
+     * @param  string  $delim
+     * @return \Lazy\Db\Query\Builder
+     */
+    public function having(string $col, $op, $val = null, string $delim = 'and'): Builder
+    {
+        [$op, $val] = $this->prepareOpAndVal($op, $val);
+
+        $type = 'Simple';
+        $this->havings[] = compact('type', 'col', 'op', 'val');
+
+        return $this;
+    }
+
+    /**
+     * or having...
+     *
+     * @param  string  $col
+     * @param  mixed  $op
+     * @param  mixed|null  $val
+     * @return \Lazy\Db\Query\Builder
+     */
+    public function orHaving(string $col, $op, $val = null): Builder
+    {
+        return $this->having($col, $op, $val, 'or');
+    }
 }
