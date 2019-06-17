a73455da53b611ad7a31b0ba4c3267a9e95dce5b
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-17 12:44:55 +0300

    Update WhereTrait.php

--- src/Db/Query/WhereTrait.php
+++ src/Db/Query/WhereTrait.php
@@ -4,6 +4,11 @@ namespace Lazy\Db\Query;
 
 use Closure;
 
+/**
+ * @property string $db
+ * @property string $table
+ * @property \Lazy\Db\Query\CompilerInterface $compiler
+ */
 trait WhereTrait
 {
     /**
@@ -47,5 +52,59 @@ trait WhereTrait
         $this->wheres[] = compact('type', 'col', 'op', 'val', 'delim', 'not');
 
         return $this;
+    }
+
+    /**
+     * where ( ... ) ...
+     *
+     * @param  \Closure  $callback
+     * @param  string  $delim
+     * @param  bool  $not
+     * @return \Lazy\Db\Query\Builder
+     */
+    public function whereGroup(Closure $callback, string $delim = 'and', bool $not = false): Builder
+    {
+        $query = new static($this->db, $this->table, $this->compiler);
+
+        $callback($query);
+
+        $wheres = $query->wheres;
+
+        $type = 'Group';
+        $this->wheres[] = compact('type', 'wheres', 'delim', 'not');
+
+        return $this;
+    }
+
+    /**
+     * where ( select ... ) ...
+     *
+     * @param  \Closure  $callback
+     * @param  string  $delim
+     * @param  bool  $not
+     * @return \Lazy\Db\Query\Builder
+     */
+    public function whereSelect(Closure $callback, string $delim = 'and', bool $not = false): Builder
+    {
+        $query = new static(null, null, $this->compiler);
+
+        $callback($query);
+
+        $type = 'Group';
+        $this->wheres[] = compact('type', 'query', 'delim', 'not');
+
+        return $this;
+    }
+
+    /**
+     * Prepare an operator and a value.
+     *
+     * @param  mixed  $op
+     * @param  mixed  $val
+     * @return mixed[]
+     */
+    protected function prepareOpAndVal($op, $val): array
+    {
+        return (null === $val) ? ['=', $op] : [$op, $val];
     }
 }
