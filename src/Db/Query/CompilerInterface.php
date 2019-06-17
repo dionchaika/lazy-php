9f6b76c5fb8fbb4fc272d957ca0711e01b1617b0
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-17 16:00:06 +0300

    Update CompilerInterface.php

--- src/Db/Query/CompilerInterface.php
+++ src/Db/Query/CompilerInterface.php
@@ -11,4 +11,28 @@ interface CompilerInterface
      * @return string
      */
     public function compileSelect(Builder $query): string;
+
+    /**
+     * Compile a query insert statement.
+     *
+     * @param  \Lazy\Db\Query\Builder  $query
+     * @return string
+     */
+    public function compileInsert(Builder $query): string;
+
+    /**
+     * Compile a query update statement.
+     *
+     * @param  \Lazy\Db\Query\Builder  $query
+     * @return string
+     */
+    public function compileUpdate(Builder $query): string;
+
+    /**
+     * Compile a query delete statement.
+     *
+     * @param  \Lazy\Db\Query\Builder  $query
+     * @return string
+     */
+    public function compileDelete(Builder $query): string;
 }
