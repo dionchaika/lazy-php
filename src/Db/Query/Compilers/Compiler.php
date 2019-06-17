830b1bd9f9987acc6a357f7661bfb5c973afcf2c
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-17 12:00:30 +0300

    Some fixes

--- src/Db/Query/Compilers/Compiler.php
+++ src/Db/Query/Compilers/Compiler.php
@@ -2,6 +2,7 @@
 
 namespace Lazy\Db\Query\Compilers;
 
+use Lazy\Db\Query\Raw;
 use Lazy\Db\Query\Builder;
 use Lazy\Db\Query\CompilerInterface;
 
@@ -13,8 +14,41 @@ class Compiler implements CompilerInterface
     /**
      * {@inheritDoc}
      */
-    public function compileWhere(Builder $builder): string
+    public function compileSelect(Builder $builder): string
     {
-        
+        $cols = $builder->cols;
+        if (empty($cols)) {
+            $cols[] = '*';
+        }
+
+        $sql = $builder->distinct ? 'select distinct ' : 'select ';
+
+        $sql .= $this->compileCols($builder, $cols);
+
+        $sql .= ' from '.$builder->table;
+
+        return $sql.';';
+    }
+
+    /**
+     * Compile columns.
+     *
+     * @param  \Lazy\Db\Query\Builder  $builder
+     * @param  string[]  $cols
+     * @return string
+     */
+    protected function compileCols(Builder $builder, array $cols): string
+    {
+        return implode(', ', array_map(function ($col) use ($builder) {
+            if ($col instanceof Raw) {
+                return (string) $col;
+            }
+
+            if ($builder->db) {
+                $col = $builder->db.'.'.$col;
+            }
+
+            return $col;
+        }, $cols));
     }
 }
