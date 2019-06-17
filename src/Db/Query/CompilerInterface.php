cc91e1a20da28d5f16fccad4083a98cb69517e57
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-17 13:21:49 +0300

    Some addings

--- src/Db/Query/CompilerInterface.php
+++ src/Db/Query/CompilerInterface.php
@@ -5,7 +5,7 @@ namespace Lazy\Db\Query;
 interface CompilerInterface
 {
     /**
-     * Compile a select statement.
+     * Compile select statement.
      *
      * @param  \Lazy\Db\Query\Builder  $query
      * @return string
--- src/Db/Query/Compilers/Compiler.php
+++ src/Db/Query/Compilers/Compiler.php
@@ -2,9 +2,68 @@
 
 namespace Lazy\Db\Query\Compilers;
 
+use RuntimeException;
+use Lazy\Db\Query\Raw;
+use Lazy\Db\Query\Builder;
 use Lazy\Db\Query\CompilerInterface;
 
 /**
  * The query compiler base class.
  */
-class Compiler implements CompilerInterface {}
+class Compiler implements CompilerInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function compileSelect(Builder $query): string
+    {
+        if (! $query->table) {
+            throw new RuntimeException('Invalid query! Table is not defined.');
+        }
+
+        $sql = $query->distinct ? 'select distinct' : 'select';
+
+        $sql .= implode(' ', [
+
+            $this->compileCols($query),
+            $this->compileFrom($query),
+            $this->compileWhere($query)
+
+        ]).';';
+
+        return $sql;
+    }
+
+    /**
+     * Compile columns.
+     *
+     * @param  \Lazy\Db\Query\Builder  $query
+     * @return string
+     */
+    protected function compileCols(Builder $query): string
+    {
+
+    }
+
+    /**
+     * Compile from.
+     *
+     * @param  \Lazy\Db\Query\Builder  $query
+     * @return string
+     */
+    protected function compileFrom(Builder $query): string
+    {
+
+    }
+
+    /**
+     * Compile where.
+     *
+     * @param  \Lazy\Db\Query\Builder  $query
+     * @return string
+     */
+    protected function compileWhere(Builder $query): string
+    {
+
+    }
+}
