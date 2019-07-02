a13a730beb7cff189e9c1b301778045227d47310
The Lazy PHP Framework <52075346+thelazyphp@users.noreply.github.com>
2019-07-02 16:43:58 +0300

    Update functions.php

--- src/Http/functions.php
+++ src/Http/functions.php
@@ -179,43 +179,16 @@ if (! function_exists('to_stream')) {
      */
     function to_stream($resource = '', array $opts = []): Stream
     {
-        if (is_callable($resource)) {
-            return to_stream(call_user_func($resource), $opts);
-        }
-
         if ($resource instanceof StreamInterface) {
             return $resource;
         }
 
-        $type = gettype($resource);
-
-        try {
-            if ('resource' === $type || 'resource (closed)' === $type) {
-                return new Stream($resource, $opts);
-            }
-
-            if ('NULL' === $type) {
-                return new Stream(fopen('php://temp', 'r+'), $opts);
-            }
-
-            if ('string' === $type) {
-                return new Stream(fwrite(fopen('php://temp', 'r+'), $resource), $opts);
-            }
-
-            if (
-                'boolean' || 'integer' || 'float'
-                || ('object' === $type && method_exists($resource, '__toString'))
-            ) {
-                return new Stream(fwrite(fopen('php://temp', 'r+'), (string) $resource), $opts);
-            }
-
-            if ('array' === $type) {
-                return new Stream(fwrite(fopen('php://temp', 'r+'), print_r($resource, true)), $opts);
-            }
-        } catch (Throwable $e) {
-            throw new RuntimeException("Unable to create a stream from resource: {$e->getMessage()}!");
+        if (is_callable($resource)) {
+            return to_stream(call_user_func($resource), $opts);
         }
 
-        throw new InvalidArgumentException("Invalid type of the resource: {$type}!");
+        $type = gettype($resource);
+
+        
     }
 }
