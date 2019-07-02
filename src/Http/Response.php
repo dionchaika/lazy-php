411a268d7fa3fbdc21d5ba3664c32af7f9aabd15
The Lazy PHP Framework <52075346+thelazyphp@users.noreply.github.com>
2019-07-02 15:22:43 +0300

    Update Response.php

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -238,7 +238,7 @@ class Response extends Message implements ResponseInterface
     }
 
     /**
-     * Check is the response informational response.
+     * Check is the response an informational response.
      *
      * @return bool
      */
@@ -248,7 +248,7 @@ class Response extends Message implements ResponseInterface
     }
 
     /**
-     * Check is the response successful response.
+     * Check is the response a successful response.
      *
      * @return bool
      */
