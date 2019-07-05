6bd5278fb57cc5bcd6a5ca986c4f759bb90d10ef
The Lazy PHP Framework <52075346+thelazyphp@users.noreply.github.com>
2019-07-05 12:17:58 +0300

    Update FormDataStream.php

--- src/Http/FormDataStream.php
+++ src/Http/FormDataStream.php
@@ -19,6 +19,36 @@ class FormDataStream extends Stream implements StreamInterface
     const BOUNDARY_CHARSET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
     /**
+     * The multipart/form-data stream boundary.
+     *
+     * @var string
+     */
+    protected $boundary;
+
+    /**
+     * The multipart/form-data stream constructor.
+     *
+     * @param  mixed[]  $parts
+     * @param  string|null  $boundary
+     */
+    public function __construct(array $parts = [], $boundary = null)
+    {
+        parent::__construct(fopen('php://temp', 'r+'));
+
+        $this->boundary = $boundary ?? $this->generateBoundary();
+    }
+
+    /**
+     * Get the multipart/form-data stream boundary.
+     *
+     * @return string
+     */
+    public function getBoundary()
+    {
+        return $this->boundary;
+    }
+
+    /**
      * Generate a multipart/form-data boundary.
      *
      * @param  int  $length
