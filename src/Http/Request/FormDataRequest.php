b208533c34461386fb188466d0a361d422c68768
The Lazy PHP Framework <52075346+thelazyphp@users.noreply.github.com>
2019-07-05 11:30:12 +0300

    Update FormDataRequest.php

--- src/Http/Request/FormDataRequest.php
+++ src/Http/Request/FormDataRequest.php
@@ -3,15 +3,15 @@
 namespace Lazy\Http\Request;
 
 use Lazy\Http\Request;
+use Lazy\Http\FormData;
 use Psr\Http\Message\RequestInterface;
 
 class FormDataRequest extends Request implements RequestInterface
 {
     /**
-     * The form-data request constructor.
+     * The multipart/form-data request constructor.
      *
-     * @param  mixed[]  $data
-     * @param  string|null  $boundary
+     * @param  \Lazy\Http\FormData|mixed[]  $data
      * @param  string  $method
      * @param  \Psr\Http\Message\UriInterface|string|null  $uri
      * @param  mixed[]  $headers
@@ -19,24 +19,21 @@ class FormDataRequest extends Request implements RequestInterface
      *
      * @throws \InvalidArgumentException
      */
-    public function __construct(array $data,
-                                $boundary = null,
+    public function __construct($data,
                                 $method = Method::GET,
                                 $uri = null,
                                 array $headers = [],
                                 $protocolVersion = '1.1')
     {
+        $data = ($data instanceof FormData)
+            ? $data
+            : new FormData($data);
 
-    }
+        $body = $data->getStream();
 
-    /**
-     * Generate a form-data boundary.
-     *
-     * @param  string  $prefix
-     * @return void
-     */
-    public function generateBoundary($prefix = '')
-    {
-        
+        parent::__construct($method, $uri, $headers, $body, $protocolVersion);
+
+        $this->setContentTypeHeader('multipart/form-data');
+        $this->setContentLengthHeader((string) $body->getSize());
     }
 }
