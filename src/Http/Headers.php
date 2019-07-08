3b892552fd73e5317dbeddae8383e2f47ea2e562
The Lazy PHP Framework <52075346+thelazyphp@users.noreply.github.com>
2019-07-08 12:51:53 +0300

    Create Headers.php

---
+++ src/Http/Headers.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Lazy\Http;
+
+class Headers
+{
+    protected $headers = [];
+
+    public function all()
+    {
+        foreach ($this->headers as $header)
+        {
+            $headers[$header['name']] = $header['value'];
+        }
+
+        return $headers;
+    }
+
+    public function has($name)
+    {
+        return isset($this->headers[strtolower($name)]);
+    }
+
+    public function get($name)
+    {
+        return $this->has($name) ? $this->headers[strtolower($name)] : [];
+    }
+
+    public function getLine($name)
+    {
+        return implode(', ', $this->get($name));
+    }
+
+    public function set($name, $value)
+    {
+        $value = (array) $value;
+
+        $this->headers[strtolower($name)] = compact('name', 'value');
+
+        return $this;
+    }
+
+    public function add($name, $value)
+    {
+        if (! $this->has($name)) {
+            $this->set($name, $value);
+        } else {
+            $this->headers[strtolower($name)]['value'] = array_merge(
+                $this->headers[strtolower($name)]['value'], (array) $value
+            );
+        }
+
+        return $this;
+    }
+
+    public function remove($name)
+    {
+        unset($this->headers[strtolower($name)]);
+    }
+}
