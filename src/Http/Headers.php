6073a32649eecbdadf6953881f1a17d815332c4f
The Lazy PHP Framework <52075346+thelazyphp@users.noreply.github.com>
2019-07-08 13:09:35 +0300

    Update Headers.php

--- src/Http/Headers.php
+++ src/Http/Headers.php
@@ -4,8 +4,35 @@ namespace Lazy\Http;
 
 class Headers
 {
+    /**
+     * The array of all of the headers.
+     *
+     * Note: The array keys
+     * are the lowercased header names while
+     * the array values contains the original
+     * header name and the array of header values.
+     *
+     * @var mixed[]
+     */
     protected $headers = [];
 
+    /**
+     * Get the raw array
+     * of all of the headers in the collection.
+     *
+     * @return mixed[]
+     */
+    public function raw()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * Get the array
+     * of all of the headers in the collection.
+     *
+     * @return mixed[]
+     */
     public function all()
     {
         foreach ($this->headers as $header) {
@@ -15,21 +42,48 @@ class Headers
         return $headers;
     }
 
+    /**
+     * Check is the header exists in the collection.
+     *
+     * @param  string  $name
+     * @return bool
+     */
     public function has($name)
     {
         return isset($this->headers[strtolower($name)]);
     }
 
+    /**
+     * Get the header from the collection.
+     *
+     * @param  string  $name
+     * @return string[]
+     */
     public function get($name)
     {
-        return $this->has($name) ? $this->headers[strtolower($name)] : [];
+        $name = strtolower($name);
+
+        return isset($this->headers[$name]) ? $this->headers[$name] : [];
     }
 
+    /**
+     * Get the header line from the collection.
+     *
+     * @param  string  $name
+     * @return string
+     */
     public function getLine($name)
     {
         return implode(', ', $this->get($name));
     }
 
+    /**
+     * Set header in the collection.
+     *
+     * @param  string  $name
+     * @param  string|string[]  $value
+     * @return self
+     */
     public function set($name, $value)
     {
         $value = (array) $value;
@@ -39,19 +93,30 @@ class Headers
         return $this;
     }
 
+    /**
+     * Add header to the collection.
+     *
+     * @param  string  $name
+     * @param  string|string[]  $value
+     * @return self
+     */
     public function add($name, $value)
     {
-        if (! $this->has($name)) {
-            $this->set($name, $value);
-        } else {
-            $this->headers[strtolower($name)]['value'] = array_merge(
-                $this->headers[strtolower($name)]['value'], (array) $value
-            );
+        $normalizedName = strtolower($name);
+
+        if (! isset($this->headers[$normalizedName])) {
+            $this->headers[$normalizedName] = compact('name', 'value');
         }
 
-        return $this;
+        $this->headers[$normalizedName] = array_merge($this->headers[$normalizedName], (array) $value);
     }
 
+    /**
+     * Remove header from the collection.
+     *
+     * @param  string  $name
+     * @return void
+     */
     public function remove($name)
     {
         unset($this->headers[strtolower($name)]);
