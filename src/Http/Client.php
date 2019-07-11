65ad369b28ef058d4700978920ef2b2b5387bd12
The Lazy PHP Framework <52075346+thelazyphp@users.noreply.github.com>
2019-07-11 11:35:50 +0300

    Update Client.php

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -4,6 +4,8 @@ namespace Lazy\Http;
 
 use ArrayAccess;
 use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * The PSR-18 HTTP client implementation class.
@@ -17,7 +19,16 @@ class Client implements ArrayAccess, ClientInterface
      */
     const DEFAULT_CONFIG_OPTIONS = [
 
-        'headers' => []
+        'headers' => [],
+        'redirect' => [
+
+            'max'     => 10,
+            'strict'  => true,
+            'referer' => true,
+            'history' => true,
+            'schemes' => ['https', 'https']
+
+        ]
 
     ];
 
@@ -35,6 +46,56 @@ class Client implements ArrayAccess, ClientInterface
      */
     public function __construct(array $config = [])
     {
+        $this->config = $this->setConfig($config);
+    }
+
+    /**
+     * Set an array of client config options.
+     *
+     * @param  array  $config
+     * @return void
+     */
+    public function setConfig(array $config)
+    {
         $this->config = array_merge(static::DEFAULT_CONFIG_OPTIONS, $config);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function sendRequest(RequestInterface $request): ResponseInterface
+    {
+        //
+    }
+
+    /**
+     * Make an HTTP request.
+     *
+     * @param  string  $method
+     * @param  \Psr\Http\Message\UriInterface|string  $uri
+     * @param  array  $opts
+     * @return \Psr\Http\Message\ResponseInterface
+     *
+     * @throws \Psr\Http\Client\ClientExceptionInterface
+     */
+    public function request($method = Method::GET, $uri = '/', $opts = []): ResponseInterface
+    {
+        $request = new Request($method, $uri);
+
+        if (isset($opts['data'])) {
+            $request = $request->withBody(
+                create_stream($opts['data'])
+            );
+        } else if (isset($opts['xml'])) {
+            $request = $request->withXml($opts['xml']);
+        } else if (isset($opts['json'])) {
+            $request = $request->withJson($opts['json']);
+        } else if (isset($opts['form_data'])) {
+            $request = $request->withFormData($opts['form_data']);
+        } else if (isset($opts['urlencoded'])) {
+            $request = $request->withUrlencoded($opts['urlencoded']);
+        }
+
+        return $this->sendRequest($request);
     }
 }
