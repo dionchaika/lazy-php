626351eef200a836b0914933c8654770184b6c7f
The Lazy PHP Framework <52075346+thelazyphp@users.noreply.github.com>
2019-07-12 14:29:02 +0300

    Update Connection.php

--- src/Db/Connection.php
+++ src/Db/Connection.php
@@ -3,6 +3,8 @@
 namespace Lazy\Db;
 
 use PDO;
+use Closure;
+use Throwable;
 use PDOStatement;
 
 /**
@@ -23,6 +25,13 @@ class Connection implements ConnectionInterface
      * @var array
      */
     protected $config = [];
+
+    /**
+     * The number of current transaction savepoint.
+     *
+     * @var int
+     */
+    protected $savepointNumber = 0;
 
     /**
      * The database connection constructor.
@@ -67,6 +76,70 @@ class Connection implements ConnectionInterface
     /**
      * {@inheritDoc}
      */
+    public function commit()
+    {
+        if (1 === $this->savepointNumber) {
+            $this->pdo->commit();
+        }
+
+        $this->savepointNumber = max(0, $this->savepointNumber - 1);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function rollBack()
+    {
+        if (1 === $this->savepointNumber) {
+            $this->pdo->rollBack();
+        } else if ($this->isSupportsSavepoints()) {
+            $this->pdo->exec(
+                $this->getSqlForSavepointRollBack('savepoint'.$this->savepointNumber)
+            );
+        }
+
+        $this->savepointNumber = max(0, $this->savepointNumber - 1);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function beginTransaction()
+    {
+        if (0 === $this->savepointNumber) {
+            $this->pdo->beginTransaction();
+        } else if ($this->isSupportsSavepoints()) {
+            $this->pdo->exec(
+                $this->getSqlForSavepoint('savepoint'.$this->savepointNumber)
+            );
+        }
+
+        $this->savepointNumber++;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function transaction(Closure $callback)
+    {
+        $this->beginTransaction();
+
+        try {
+            $result = call_user_func($callback, $this);
+
+            $this->commit();
+
+            return $result;
+        } catch (Throwable $e) {
+            $this->rollBack();
+
+            throw $e;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public function bindValues(PDOStatement $statement, $bindings = [])
     {
         $bindings = (array) $bindings;
@@ -76,5 +149,38 @@ class Connection implements ConnectionInterface
                 is_int($key) ? $key + 1 : $key, $value, is_int($value) ? PDO::PARAM_INT : PDO::PARAM_STR
             );
         }
+    }
+
+    /**
+     * Check is the database
+     * connection supports savepoints.
+     *
+     * @return bool
+     */
+    protected function isSupportsSavepoints()
+    {
+        return true;
+    }
+
+    /**
+     * Get an SQL for database connection savepoint.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function getSqlForSavepoint($name)
+    {
+        return 'SAVEPOINT '.$name;
+    }
+
+    /**
+     * Get an SQL for database connection savepoint roll back.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function getSqlForSavepointRollBack($name)
+    {
+        return 'ROLLBACK TO SAVEPOINT '.$name;
     }
 }
