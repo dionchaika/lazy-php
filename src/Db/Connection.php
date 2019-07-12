09d1bb58238c89042aca39f3c4f8521bdf668fb5
The Lazy PHP Framework <52075346+thelazyphp@users.noreply.github.com>
2019-07-12 13:24:34 +0300

    Update Connection.php

--- src/Db/Connection.php
+++ src/Db/Connection.php
@@ -3,6 +3,8 @@
 namespace Lazy\Db;
 
 use PDO;
+use Closure;
+use Exception;
 use PDOException;
 use PDOStatement;
 
@@ -68,6 +70,52 @@ class Connection implements ConnectionInterface
     /**
      * {@inheritDoc}
      */
+    public function select($sql, $bindings = [])
+    {
+        return $this->statement($sql, $bindings)->fetchAll();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function insert($sql, $bindings = [])
+    {
+        return $this->affectingStatement($sql, $bindings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function update($sql, $bindings = [])
+    {
+        return $this->affectingStatement($sql, $bindings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function delete($sql, $bindings = [])
+    {
+        return $this->affectingStatement($sql, $bindings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function execute($sql, $bindings = [])
+    {
+        return $this->executeCallback($sql, $bindings, function ($sql, $bindings) {
+            $statement = $this->pdo->prepare($sql);
+
+            $this->bindValues($statement, $bindings);
+
+            return $statement->execute();
+        });
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public function bindValues(PDOStatement $statement, $bindings = [])
     {
         $bindings = (array) $bindings;
@@ -77,5 +125,76 @@ class Connection implements ConnectionInterface
                 is_int($key) ? $key + 1 : $key, $value, is_int($value) ? PDO::PARAM_INT : PDO::PARAM_STR
             );
         }
+    }
+
+    /**
+     * Excecute a statement.
+     *
+     * @param  string  $sql
+     * @param  mixed|array  $bindings
+     * @return \PDOStatement
+     *
+     * @throws \Exception
+     */
+    protected function statement($sql, $bindings = []): PDOStatement
+    {
+        return $this->executeCallback($sql, $bindings, function ($sql, $bindings) {
+            $statement = $this->pdo->prepare($sql);
+
+            $this->bindValues($statement, $bindings);
+
+            $statement->execute();
+
+            return $statement;
+        });
+    }
+
+    /**
+     * Excecute a statement and return the number of affected rows.
+     *
+     * @param  string  $sql
+     * @param  mixed|array  $bindings
+     * @return int
+     *
+     * @throws \Exception
+     */
+    protected function affectingStatement($sql, $bindings = [])
+    {
+        return $this->executeCallback($sql, $bindings, function ($sql, $bindings) {
+            $statement = $this->pdo->prepare($sql);
+
+            $this->bindValues($statement, $bindings);
+
+            $statement->execute();
+
+            return $statement->rowCount();
+        });
+    }
+
+    /**
+     * Execute a statement within the callback.
+     *
+     * @param  string  $sql
+     * @param  mixed|array  $bindings
+     * @param  \Closure  $callback
+     * @return mixed
+     *
+     * @throws \Exception
+     */
+    protected function executeCallback($sql, $bindings = [], Closure $callback)
+    {
+        $startTime = microtime(true);
+
+        try {
+            $result = call_user_func(
+                $callback, $sql, $bindings
+            );
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
+
+        $executionTime = microtime(true) - $startTime;
+
+        return is_numeric($result) ? (int) $result : $result;
     }
 }
